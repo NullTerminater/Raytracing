0d6ea058a54b103a3c3d3ef057ad9c2c208de530
Javidan <javidanaghayev1@gmail.com>
2026-01-31 20:00:05 -0600

    We can now draw a circle

--- raytracing.c
+++ raytracing.c
@@ -8,13 +8,16 @@
 #define HEIGHT 600
 #define COLOR_WHITE 0xffffffff
 
-typedef struct {
+typedef struct
+{
     double x;
     double y;
     double r;
 } Circle;
 
-int main (void) {
+
+int main(void)
+{
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
@@ -22,28 +25,39 @@ int main (void) {
     // SDL_FillRect(surface, &rect, COLOR_WHITE);
     int quit = 0;
 
-
     Circle light_source;
-    for(int i = 0; i < 100; i++){
-        for(int j = 0; j < 100; j++){
-            SDL_Rect little_rect = (SDL_Rect) {i, j, 1, 1};
-            SDL_FillRect(surface, &little_rect, COLOR_WHITE);
+    light_source.r = 50;
+
+    double dist_squared;
+
+    //draws circle
+    for (int i = 0; i < light_source.r * 2; i++)
+    {
+        for (int j = 0; j < light_source.r * 2; j++)
+        {
+            dist_squared = pow(i - light_source.r ,2) + pow(j - light_source.r, 2);
+            if (dist_squared <= pow(light_source.r, 2))
+            {
+                SDL_Rect little_rect = (SDL_Rect){i, j, 1, 1};
+                SDL_FillRect(surface, &little_rect, COLOR_WHITE);
+            }
         }
     }
 
     SDL_UpdateWindowSurface(window);
 
     SDL_Event e;
-    while (!quit) {
-        while(SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
                 quit = 1;
             }
         }
     }
     SDL_DestroyWindow(window);
-
-
 
     return 0;
 }
