d222cd9b46bdc7f53cfa19799a9bff92d1b1dbde
Javidan <javidanaghayev1@gmail.com>
2026-02-04 22:04:09 -0600

    Working on lightray logic

--- raytracing.c
+++ raytracing.c
@@ -6,8 +6,12 @@
 
 #define WIDTH 900
 #define HEIGHT 600
+#define MAX_FPS 60
+#define SCREEN_TICKS_PER_FRAME 1000
 #define COLOR_WHITE 0xffffffff
 #define COLOR_BLACK 0x00000000
+#define NUM_RAYS 100
+
 
 typedef struct
 {
@@ -32,6 +36,22 @@ void draw_circle(Circle light_source, SDL_Surface *surface, int OFFSET_X, int OF
         }
     }
 }
+
+void draw_ray(SDL_Surface *surface, int x, int y, int pixel_width){
+    // while not out of bounds
+    int x_pos = 0;
+    int y_pos = 0;
+    while((x > 0 && x < WIDTH) && (y > 0 && y < HEIGHT)) {
+        SDL_Rect wave_rect = (SDL_Rect){cos(x), sin(y), pixel_width, pixel_width};
+        // x_pos = cos(x);
+        // y_pos = sin(y);
+        printf("cos(%d): %f\tsin(%d): %f\n", x, cos(x), y, sin(y));
+        SDL_FillRect(surface, &wave_rect, COLOR_WHITE ); 
+        x++;
+        y++; 
+    }
+
+}
 int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -39,18 +59,19 @@ int main(void)
     SDL_Surface *surface = SDL_GetWindowSurface(window);
     // SDL_Rect rect = (SDL_Rect) {0, 0, 200, 200};
     // SDL_FillRect(surface, &rect, COLOR_WHITE);
-    int quit = 0;
-
+    
+    // draws circle
     Circle light_source;
     light_source.r = 50;
-    // draws circle
     draw_circle(light_source, surface, 200, 200);
 
     SDL_UpdateWindowSurface(window);
 
-    SDL_Rect rect = (SDL_Rect){0, 0, WIDTH, HEIGHT};
+    SDL_Rect full_rect = (SDL_Rect){0, 0, WIDTH, HEIGHT};
+    SDL_Rect wave_rect = (SDL_Rect){10, 10, 1, 1};
     SDL_Event e;
-    while (!quit)
+    int is_running = 1;
+    while (is_running)
     {
         while (SDL_PollEvent(&e) != 0)
         {
@@ -58,15 +79,16 @@ int main(void)
             SDL_GetMouseState(&x, &y);
             if (e.type == SDL_MOUSEMOTION )
             {
-                // printf("x: %d y: %d\n", x, y);
-                SDL_FillRect(surface, &rect, COLOR_BLACK);
+                SDL_FillRect(surface, &full_rect, COLOR_BLACK);
                 draw_circle(light_source, surface, x, y);
+                SDL_FillRect(surface, &wave_rect, COLOR_WHITE);
+                draw_ray(surface, 100, 100, 1);
                 // draw_circle(light_source, surface, x - light_source.r, y - light_source.r);
                 SDL_UpdateWindowSurface(window);
             }
             else if (e.type == SDL_QUIT)
             {
-                quit = 1;
+                is_running = 0;
             }
         }
     }
@@ -74,3 +96,4 @@ int main(void)
 
     return 0;
 }
+
